791c92eccccac0426394eb9254444bf8b8d0723f
Kysan721 <kyshu.senpai@gmail.com>
2020-12-23 22:44:26 +0100

    removed __pychache__

---
+++ test/config.py
@@ -0,0 +1,2 @@
+
+config = {'bot_token': 'NzkxNDA4MDQ2MTM1NDQzNTI4.X-OuLQ.q16bcOZ7iHJStTJOd8h_30FCKC8'}
--- test/main.py
+++ test/main.py
@@ -1,11 +1,12 @@
 # pour tester un bot sans le compiler
+from .config import config
 
 if __name__ == '__main__':
     import sys
     sys.path[0] += ("/..")
-    # print(sys.path)
+    # print(sys.path[0])
     from src.Bot import Bot
     sys.path[0] = sys.path[0][:-3]
     bot = Bot()
     # token qui devras être généré à la volé durant la création d'un nouveau client
-    bot.run('NzkxNDA4MDQ2MTM1NDQzNTI4.X-OuLQ.q16bcOZ7iHJStTJOd8h_30FCKC8')
+    bot.run(config['bot_token'])
